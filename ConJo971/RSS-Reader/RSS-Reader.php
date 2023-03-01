64140510a5817aefb3ec78f36f23e9474883e329
Jonathan20000 <ConJo971@school.lu>
2023-03-01 14:51:52 +0100

    rss-scraper

--- ConJo971/RSS-Reader/RSS-Reader.php
+++ ConJo971/RSS-Reader/RSS-Reader.php
@@ -1,27 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-</head>
-<body>
 <?php
+
 $string = file_get_contents('https://www.rtl.lu/rss/headlines');
 
 $xml = simplexml_load_string($string);
 
-foreach ($xml->channel->item as $item) {
-    echo "<div>";
-        echo "<h2>" . $item->title . "</h2>";
-        echo "<p>" . $item->description . "</p>";
-        echo "<br>";
-        echo "<p>" . $item->pubDate . "</p>";
-    echo "</div>";
+$body = "";
 
+foreach ($xml->channel->item as $item) {
+    $body .= "<div>";
+       $body .= "<h1>" . $item->title . "</h1>";
+       $body .= "<p>" . $item->description . "</p>";
+       $body .= "<p>" . $item->pubDate . "</p>";
+    $body .= "</div>";
 }
 
-
+echo "$body";
 
 ?>
-</body>
+
