94c9d1b90b855c89cbe5a97607c80dbf309791d6
Jonathan20000 <ConJo971@school.lu>
2023-03-02 11:13:47 +0100

    Update RSS-Reader.php

--- ConJo971/RSS-Reader/RSS-Reader.php
+++ ConJo971/RSS-Reader/RSS-Reader.php
@@ -4,13 +4,18 @@ $string = file_get_contents('https://www.rtl.lu/rss/headlines');
 
 $xml = simplexml_load_string($string);
 
+
 $body = "";
 
 foreach ($xml->channel->item as $item) {
+    //echo "<pre>";
+    //    print_r($item->enclosure->@attributes);
+    //echo "</pre>";
     $body .= "<div>";
-       $body .= "<h1>" . $item->title . "</h1>";
-       $body .= "<p>" . $item->description . "</p>";
-       $body .= "<p>" . $item->pubDate . "</p>";
+        $body .= "<h1>" . $item->title . "</h1>";
+        $body .= "<p>" . $item->description . "</p>";
+        $body .= "<p>" . $item->pubDate . "</p>";
+        //$body .= "<img src='". $item->enclosure->url ."'>";
     $body .= "</div>";
 }
 
